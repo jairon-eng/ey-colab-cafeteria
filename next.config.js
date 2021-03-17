a75705c3280cf3926a78ec9660ccb50330dae9f1
jairon-eng <jairo2198herrera@gmail.com>
2021-03-17 00:12:11 -0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        "BASE_URL": "http://localhost:3000",
+        "MONGODB_URL": "mongodb+srv://next:123@cluster2.lwq9y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "1237404422563394389",
+        "REFRESH_TOKEN_SECRET": "123secret97098382rere13063711",
+        "PAYPAL_CLIENT_ID": "Af8eKNWCAG4N63U6deNc4lwIFw3FuXaeCibfzAo6V08HVQ6Jpu2LoXzQE39n4jLtmlZsuPwhwU7xFr2g",
+        "CLOUD_UPDATE_PRESET": "cafeteria",
+        "CLOUD_NAME": "dwkgbsypm",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/dwkgbsypm/image/upload"
+    }
+}
